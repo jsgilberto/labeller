6f8ad13cd4630a512ce95af0bf35ed79763c5b6c
jsgilberto <alv.mtz94@gmail.com>
2018-06-14 01:18:12 +0200

    basic functionality of loading images

---
+++ helpers.js
@@ -0,0 +1,84 @@
+// Removes the options from the html document (not from local database)
+function removeOptions(){
+  var options = document.getElementsByTagName("option");
+  
+	for(var i = options.length - 1; i >= 0; i--){
+		options[i].parentNode.removeChild(options[i]);
+	}
+}
+
+// Shows images stored in local database
+function showOptions(){
+	for ( var i = 0, len = localStorage.length; i < len; ++i ) {
+		console.log( localStorage.getItem( localStorage.key( i ) ) );
+		createElement("file-list", "OPTION", localStorage.getItem(localStorage.key(i)), localStorage.key(i));
+	}
+
+	var options = document.getElementsByTagName("option");
+	
+	for (var i = 0; i < options.length; i++){
+		options[i].onclick = function(){
+			img_target = localStorage.getItem(this.innerHTML);
+		}
+	}
+}
+
+// Adds an element to the document
+function createElement(parentId, elementTag, elementValue, html) {
+	var p = document.getElementById(parentId);
+	var newElement = document.createElement(elementTag);
+	if (elementValue){
+		newElement.setAttribute('value', elementValue);
+	}
+	newElement.innerHTML = html;
+	p.appendChild(newElement);
+}
+
+// Store images in localStorage variable
+function storeFiles(){
+  for(var i = 0; i < file.files.length; i++){
+
+		var fileObj = {
+			name: file.files[i].name,
+      file: URL.createObjectURL(file.files[i])
+		};
+		
+		arrayOfFiles.push(fileObj);
+		localStorage.setItem(fileObj.name, fileObj.file);
+	}
+}
+
+// Fit image in canvas
+var fitImageOn = function(canvas, imageObj) {
+  // source: https://sdqali.in/blog/2013/10/03/fitting-an-image-in-to-a-canvas-object/
+	var imageAspectRatio = imageObj.width / imageObj.height;
+	var canvasAspectRatio = canvas.width / canvas.height;
+	var renderableHeight, renderableWidth, xStart, yStart;
+
+	// If image's aspect ratio is less than canvas's we fit on height
+	// and place the image centrally along width
+	if(imageAspectRatio < canvasAspectRatio) {
+		renderableHeight = canvas.height;
+		renderableWidth = imageObj.width * (renderableHeight / imageObj.height);
+		xStart = (canvas.width - renderableWidth) / 2;
+		yStart = 0;
+	}
+
+	// If image's aspect ratio is greater than canvas's we fit on width
+	// and place the image centrally along height
+	else if(imageAspectRatio > canvasAspectRatio) {
+		renderableWidth = canvas.width
+		renderableHeight = imageObj.height * (renderableWidth / imageObj.width);
+		xStart = 0;
+		yStart = (canvas.height - renderableHeight) / 2;
+	}
+
+	// Happy path - keep aspect ratio
+	else {
+		renderableHeight = canvas.height;
+		renderableWidth = canvas.width;
+		xStart = 0;
+		yStart = 0;
+	}
+	ctx.drawImage(imageObj, xStart, yStart, renderableWidth, renderableHeight);
+};
--- index.js
+++ index.js
@@ -1,63 +1,61 @@
 var file = document.getElementById("file");
-var btn = document.getElementById("btn");
 var canvas = document.getElementById("canvas");
 var ctx = canvas.getContext("2d");
 var fr = new FileReader();
 var flag = false;
 canvas.width = 500;
 canvas.height = 500;
+var btnDownload = document.getElementById('btn-download');
+var ul_files = document.getElementById("file-list");
+var btnView = document.getElementById("btn-view");
+var arrayOfFiles = [];
+var img_target;
+var img = new Image();
+
+localStorage.clear();
+
+window.onload = function(){
+	removeOptions();
+	showOptions();
+};
 
 file.onchange = function(e) {
-  var img = new Image();
-  img.src = URL.createObjectURL(e.target.files[0]); // use first selected image from input element
-  
-  img.onload = function() {
-    flag = true;
-  }
-
-  btn.onclick = function(){
-    if(flag){
-      // clear canvas
-      ctx.canvas.width = ctx.canvas.width;
-      
-      /* ctx.drawImage(img, 0, 0, img.width,    img.height,     // source rectangle
-        0, 0, img.width*wratio, img.height*hratio); // destination rectangle */
-      fitImageOn(canvas, img);
-      flag = false;
-    }
-  }
+	storeFiles();
+	removeOptions();
+	showOptions();
+	img = new Image();
+	img.src = localStorage.getItem(localStorage.key(0)); // use first selected image from input element
+	img.onload = function() {
+		flag = true;
+		ctx.canvas.width = ctx.canvas.width;
+		fitImageOn(canvas, img);
+		console.log("done loading");
+	}
 };
 
-var fitImageOn = function(canvas, imageObj) {
-  // source: https://sdqali.in/blog/2013/10/03/fitting-an-image-in-to-a-canvas-object/
-	var imageAspectRatio = imageObj.width / imageObj.height;
-	var canvasAspectRatio = canvas.width / canvas.height;
-	var renderableHeight, renderableWidth, xStart, yStart;
-
-	// If image's aspect ratio is less than canvas's we fit on height
-	// and place the image centrally along width
-	if(imageAspectRatio < canvasAspectRatio) {
-		renderableHeight = canvas.height;
-		renderableWidth = imageObj.width * (renderableHeight / imageObj.height);
-		xStart = (canvas.width - renderableWidth) / 2;
-		yStart = 0;
-	}
-
-	// If image's aspect ratio is greater than canvas's we fit on width
-	// and place the image centrally along height
-	else if(imageAspectRatio > canvasAspectRatio) {
-		renderableWidth = canvas.width
-		renderableHeight = imageObj.height * (renderableWidth / imageObj.width);
-		xStart = 0;
-		yStart = (canvas.height - renderableHeight) / 2;
-	}
-
-	// Happy path - keep aspect ratio
-	else {
-		renderableHeight = canvas.height;
-		renderableWidth = canvas.width;
-		xStart = 0;
-		yStart = 0;
-	}
-	ctx.drawImage(imageObj, xStart, yStart, renderableWidth, renderableHeight);
+img.onload = function() {
+	flag = true;
+	ctx.canvas.width = ctx.canvas.width;
+	fitImageOn(canvas, img);
+	console.log("done loading");
 };
+
+btnView.onclick = function(){
+	if (img_target){
+		img.src = img_target;
+	}
+
+	img.onload = function() {
+		flag = true;
+		ctx.canvas.width = ctx.canvas.width;
+		fitImageOn(canvas, img);
+		console.log("done loading");
+	}
+}
+
+// download image in canvas
+btnDownload.addEventListener('click', function (e) {
+	var dataURL = canvas.toDataURL('image/png');
+	console.log(dataURL);
+  btnDownload.href = dataURL;
+});
